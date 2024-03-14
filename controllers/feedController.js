2406f088a12b9dc8459310877a712e9ad812be15
karenhnlai <karenhnlai@gmail.com>
2024-03-14 12:18:10 +0800

    router and controller changes

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -2,10 +2,23 @@
 const BaseController = require("./baseController");
 
 class FeedController extends BaseController {
-  constructor(model, feedLikeModel, feedReviewModel) {
+  constructor(model, feedLikeModel, feedReviewModel, sellerModel) {
     super(model);
     this.feedLikeModel = feedLikeModel;
     this.feedReviewModel = feedReviewModel;
+    this.sellerModel = sellerModel;
+  }
+
+  async getAll(req, res) {
+    try {
+      const data = await this.model.findAll({
+        include: [this.sellerModel], // Specify the models to eagerly load
+      });
+      return res.json(data);
+    } catch (error) {
+      return res.status(400).json({ error: true, msg: error.message });
+    }
   }
 }
+
 module.exports = FeedController;
